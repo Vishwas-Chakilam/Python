d2c934be7b20ef4b63c5a12b18829d5256a401df
Vishwas_Chakilam <109941893+Vishwas-Chakilam@users.noreply.github.com>
2024-09-08 11:47:13 +0530

    01_hello_world.py

---
+++ Basics/01_hello_world.py
@@ -0,0 +1,10 @@
+# 01_hello_world.py
+
+# This is the first program you write when learning any programming language.
+# The "Hello, World!" program simply prints the text "Hello, World!" to the screen.
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
